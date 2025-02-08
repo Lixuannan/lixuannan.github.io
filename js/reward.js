c4b68e5eab535c2b817b7964e895c1311040a84a
Lixuannan <Lixuannan@users.noreply.github.com>
2025-02-08 05:56:58 +0000

    deploy: Lixuannan/blog-source@3e4ce6df8564e14fd88e4a2cde115b29c9fb506e

--- js/reward.js
+++ js/reward.js
@@ -5,7 +5,7 @@ var askRewardText = document.getElementById('askRewardText');
 const rewardedText = `“你的鼓励就是我持续更新的动力
 <br>
 祝你一路无 Error，编译见 Success，事事顺心哦 ψ(｀∇´)ψ”`;
-const befRewarded = `觉得不错的话，给点打赏吧 (✿◕‿◕✿)`;
+const befRewarded = `“觉得不错的话，给点打赏吧 (✿◕‿◕✿)”`;
 
 if(rewardBtn){
 	rewardBtn.onclick = () => {
